960eafa1d9557da911618a78665380dd76d826c0
aezzahir <aero.glowsky@gmail.com>
2024-04-22 22:44:36 +0100

    2. Measure the runtime

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 """
-Module for the task_wait_n function.
+4
 """
 import asyncio
+import random
 from typing import List
-from . import task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous coroutine that takes in two integer arguments,
-    `n` and `max_delay`.
-    It returns a list of all the delays (float values)
-    sorted in ascending order.
+    tasks 4
     """
-    delays: List[float] = []
-    for _ in range(n):
-        delay = await task_wait_random(max_delay)
-        delays.append(delay)
-    return sorted(delays)
+    listOfDelays: List[float] = []
+    for i in range(n):
+        taskRandom: float = await task_wait_random(max_delay)
+        listOfDelays.append(taskRandom)
+    return sorted(listOfDelays)
