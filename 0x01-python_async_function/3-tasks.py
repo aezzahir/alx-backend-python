f814fa66cdfcce3fa53314ec21d5f249ff53b04f
aezzahir <aero.glowsky@gmail.com>
2024-04-22 22:43:58 +0100

    2. Measure the runtime

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """
-Module for the task_wait_random function.
+Tasks 
 """
 import asyncio
-from . import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Returns an asyncio.Task object for the wait_random coroutine with
-    the given max_delay.
+     tasks 
     """
-    coroutine = wait_random(max_delay)
-    return asyncio.create_task(coroutine)
+    coro: float = wait_random(max_delay)
+    return asyncio.create_task(coro)
